408ae6706a78acdc28d5cd9582a8d236ac60441c
chealy90 <christopherhealy20@gmail.com>
2025-03-12 14:51:13 +0000

    database seeded with test data

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,21 +9,9 @@ use Cviebrock\EloquentSluggable\Sluggable;
 class Post extends Model
 {
     use HasFactory;
-    use Sluggable;
 
-    protected $fillable = ['title', 'slug', 'description', 'image_path', 'user_id'];
+    protected $fillable = ['title', 'description', 'image_path', 'user_id'];
 
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
 
-    public function sluggable(): array
-    {
-        return [
-            'slug' => [
-                'source' => 'title'
-            ]
-        ];
-    }
+
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,29 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        Post::create([
+            'title' => "Test 1",
+            'slug' => "test",
+            'description' => "This is a description for test 1",
+            'image_path' => "https://ih1.redbubble.net/image.2595320116.9420/flat,750x,075,f-pad,750x1000,f8f8f8.jpg",
+            'user_id' => 1
+        ]);
+
+
+        Post::create([
+            'title' => "Test 2",
+            'slug' => "test",
+            'description' => "This is a description for test 2",
+            'image_path' => "https://ih1.redbubble.net/image.2595320116.9420/flat,750x,075,f-pad,750x1000,f8f8f8.jpg",
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => "Test 1",
+            'slug' => "test",
+            'description' => "This is a description for test 1",
+            'image_path' => "https://ih1.redbubble.net/image.2595320116.9420/flat,750x,075,f-pad,750x1000,f8f8f8.jpg",
+            'user_id' => 1
+        ]);
     }
 }
